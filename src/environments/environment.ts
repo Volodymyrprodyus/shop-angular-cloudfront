af4be9eb0e7581e5ecf9d97764e0ec5764883f4a
Volodymyr Prodyus <Volodymyr_Prodyus@epam.com>
2023-11-10 11:58:34 +0200

    feat(task_7): update endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,18 +8,18 @@ export const environment: Config = {
   production: false,
   apiEndpoints: {
     products: 'https://j4gsy4ue76.execute-api.us-east-1.amazonaws.com/dev',
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://j4gsy4ue76.execute-api.us-east-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://8o1mv1b0h3.execute-api.us-east-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://j4gsy4ue76.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     products: true,
-    product: false,
+    product: true,
     order: false,
     import: true,
-    bff: false,
+    bff: true,
     cart: false,
   },
 };
